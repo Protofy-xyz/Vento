279bf8c86f0b765d09ba03252025fa1e33dd84cc
jcarlosn <jcarlos.norte@gmail.com>
2025-11-26 23:59:52 +0100

    If data/pages doesn't exists, download from cdn. If it fails or this step is skipped, adminpanel and cinny will build the pages if enabled.

--- prepare.js
+++ prepare.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const semver = require('semver');
+const AdmZip = require('adm-zip');
+
 const requiredVersion = '>=18.0.0';
 
 if (!semver.satisfies(process.version, requiredVersion)) {
@@ -21,3 +23,16 @@ directories.forEach(directory => {
         fs.mkdirSync(directory);
     }
 });
+
+//download data/pages if it doesn't exist
+(async () => {
+    if (!fs.existsSync('./data/pages')) {
+        fs.mkdirSync('./data/pages');
+        //download from https://github.com/Protofy-xyz/Vento/releases/download/development/vento-pages.zip it contains a zip file with the pages, to be extracted
+        //at the root of the project
+        const response = await fetch('https://github.com/Protofy-xyz/Vento/releases/download/development/vento-pages.zip');
+        const zip = await response.arrayBuffer();
+        const zipFile = new AdmZip(Buffer.from(zip));
+        zipFile.extractAllTo('.');
+    }
+})();

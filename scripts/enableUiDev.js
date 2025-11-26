a59e9b6b612ce2d50fcdb2e6d7a38d046976af51
jcarlosn <jcarlos.norte@gmail.com>
2025-11-27 00:40:56 +0100

    Remove heavy frontend (nextjs based) dependencies from the root of the monorepo. Adds a command to enable them (yarn enable-ui-dev) or you can add manually only the ones you are interested into to develop

---
+++ scripts/enableUiDev.js
@@ -0,0 +1,24 @@
+
+const fs = require('fs');
+const path = require('path');
+const dirname = path.join(__dirname, '..')
+
+const packagePath = path.join(dirname, 'package.json');
+
+// load package.json and replace workspaces with the ones we want to keep
+if (fs.existsSync(packagePath)) {
+    const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+    //check if workspaces includes apps/adminpanel and apps/chat
+    //if not, add them
+    if (!packageJson.workspaces.includes('apps/adminpanel')) {
+        packageJson.workspaces.push('apps/adminpanel');
+    }
+    if (!packageJson.workspaces.includes('apps/chat')) {
+        packageJson.workspaces.push('apps/chat');
+    }
+    fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2), 'utf8');
+    console.log('package.json workspaces have been updated');
+} else {
+    console.error('package.json not found');
+    process.exit(1);
+}
